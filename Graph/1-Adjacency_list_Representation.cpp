b60c7507a5509e81f35ee8b6c25e208ed6cdf038
Abhishek Kumar Trivedi <37017731+tecXworld@users.noreply.github.com>
2021-06-29 13:14:27 +0530

    Add files via upload

---
+++ Graph/1-Adjacency_list_Representation.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+class graph{
+    int V;
+    list <int> *l;
+
+  public:  
+   graph(int v){
+       V=v;
+       l = new list <int> [V];
+   }
+   void addedge (int i, int j, bool undirected = true){
+       l[i].push_back(j);
+       if (undirected){
+           l[j].push_back(i);
+       }
+   }
+   void printAdjList(){
+       for (int i=0; i< V;i++){
+           cout<<i<<"-->";
+
+           for (auto node:l[i]){
+               cout << node << ",";
+           }
+           cout << endl;
+       }
+   }
+};
+
+
+
+int main() {
+    graph g(6);
+    g.addedge(1,2);
+    g.addedge(3,2);
+    g.addedge(3,1);
+    g.addedge(4,2);
+    g.addedge(4,3);
+    g.addedge(4,0);
+    g.addedge(0,1);
+    g.addedge(1,5); 
+    g.addedge(0,5);
+
+    g.printAdjList();
+    return 0;
+};
